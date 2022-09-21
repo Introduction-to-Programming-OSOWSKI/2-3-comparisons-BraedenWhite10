0486c03045792e8a70fe1a64324e85d965cb82ed
BraedenWhite10 <johndo@mystma.org>
2022-09-21 08:40:19 -0500

    1

--- main.py
+++ main.py
@@ -3,22 +3,24 @@ from tkinter.tix import Y_REGION
 
 
 def greaterThan(x,y):
-    if: x> y
+    if x> y:
         return(True)
     else: return(False)
 def lessThan(x,y):
-    if: x< y
-    return(True)
-    else: return(False)
+    if x< y:
+        return(True)
+    else:
+         return(False)
 def equalTo(x,y):
-    if: x= y
-    return(True)
+    if x == y:
+        return(True)
     else: return(False)
 def greaterOrEqual(x,y):
-    if x>= y
-    return(True)
+    if x>= y:
+        return(True)
     else: return(False)
 def lessOrEqual(x,y):
-    if x<= y
-    return(True)
-    else: return(False) 
+    if x<= y:
+        return(True)
+    else:
+         return(False) 
